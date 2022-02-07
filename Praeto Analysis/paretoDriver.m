4d14a43a3d72a1fcb54246b6474a36947cd2c93d
achokshi9800 <99149812+achokshi9800@users.noreply.github.com>
2022-02-07 12:01:03 -0500

    Add files via upload

--- Praeto Analysis/paretoDriver.m
+++ Praeto Analysis/paretoDriver.m
@@ -1,10 +1,14 @@
+%Author: Aman Chokshi
+%Driver Script for Pareto Analysis
 clc; clear;
+
 combinations = generateCombinations;
 
 cost = zeros(size(combinations,1),1);
 rel = zeros(size(combinations,1),1);
 SV = zeros(size(combinations,1),1);
 
+% Gets Cost, Science Value and Reliability for all combinations
 for i = 1:size(combinations,1)
     cost(i) = getCost(combinations(i));
     rel(i) = getReliability(combinations(i));
@@ -13,4 +17,18 @@ end
 
 Pareto = struct("cost",cost,"reliability",rel,"science",SV);
 
+%Determined best combinations
+favoredCost = find(Pareto.cost >=0.2);
+favoredScience = find(Pareto.science > 4200);
+k = 1;
+for j = 1:size(favoredCost,1)
+    if ismember(favoredCost(j),favoredScience) > 0
+        favoredComb(k) = favoredCost(j);
+        k = k+1;
+    end
+end
+favoredComb = combinations(favoredComb);
+
+% Plots Results
 fig = Plot_Pareto_Analysis(Pareto)
+clear i j k favoredCost favoredScience

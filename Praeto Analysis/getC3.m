7efa1b84574ab73a884ff249acf6bcd95a008557
braden25 <54959154+braden25@users.noreply.github.com>
2022-02-16 18:42:22 -0500

    Update getC3.m

--- Praeto Analysis/getC3.m
+++ Praeto Analysis/getC3.m
@@ -9,20 +9,22 @@ function v_inf = getC3(propinfo)
 % output: 
 % v_inf = Ending velocity of spacecraft after (km/s)
 
-g = 0.00981; % gravitational constant of earth (km/s^2)
-mu = 3.896 * 10^5; % Gravitational parameter of earth (km^3 / s^-2)
-r = 500 + 6378.1; % Radius of the earth plus the desired low earth orbit altitude (km)
+%g = 0.00981; % gravitational constant of earth (km/s^2)
+%mu = 3.896 * 10^5; % Gravitational parameter of earth (km^3 / s^-2)
+%r = 500 + 6378.1; % Radius of the earth plus the desired low earth orbit altitude (km)
 
-mi  = propinfo.mPay + propinfo.mInert + propinfo.mProp; % Initial mass calculation prior to burn maneuver (kg)
-mf = propinfo.mInert + propinfo.mPay; % Final mass calculation after burn maneuver (kg)
+%mi  = propinfo.mPay + propinfo.mInert + propinfo.mProp; % Initial mass calculation prior to burn maneuver (kg)
+%mf = propinfo.mInert + propinfo.mPay; % Final mass calculation after burn maneuver (kg)
 
-dv = g * propinfo.isp * log(mi / mf); % Delta V calculation for candidate architecture
+%dv = g * propinfo.isp * log(mi / mf); % Delta V calculation for candidate architecture
 
-v_esc = sqrt(2 * mu / r); % Escape velocity 
+%v_esc = sqrt(2 * mu / r); % Escape velocity 
 
-v = dv + v_esc; % total velocity calculation (km/s)
+%v = dv + v_esc; % total velocity calculation (km/s)
 
-C3 = v^2 - 2 * mu / r; % Characteristic energy calulation 
+%C3 = v^2 - 2 * mu / r; % Characteristic energy calulation 
+
+%C3 = propinfo.c3; %get C3 values from prop code
 
 v_inf = sqrt(C3); % Velocity at infinite radius calculation (km/s)
 end

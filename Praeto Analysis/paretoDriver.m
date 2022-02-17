7da4c133b2f98db99ad5cee0d80acf4151417bc4
achokshi9800 <99149812+achokshi9800@users.noreply.github.com>
2022-02-17 17:21:22 -0500

    Update paretoDriver.m

--- Praeto Analysis/paretoDriver.m
+++ Praeto Analysis/paretoDriver.m
@@ -19,31 +19,40 @@ for i = 1:size(combinations,1)
 
     propInfo = getPropInfo(combinations(i));
     tHP(i) = timeToHP(combinations(i),propInfo,120);
+
+
+
+
+if combinations(i).Staging == "single" || combinations(i).Staging == "mult"
+    if combinations(i).LV == "div" || combinations(i).LV == "falcon"
+        SV(i) = 0;
+    end
+end
+
 end
 
 % Makes SV for combinations that take >10 years to reach HP to 0
-tHP10 = find(tHP > 10);
 
+tHP10 = find(tHP > 10);
 for l = 1:length(tHP10)
     SV(tHP10(l)) = 0;
 end
 
 
-
 Pareto = struct("cost",cost,"reliability",rel,"science",SV);
 
 
-% %Determined best combinations. Change bounds based on what is desired.
- favoredCost = find(Pareto.cost >=390);
- favoredScience = find(Pareto.science > .375);
+% % %Determined best combinations. Change bounds based on what is desired.
+ favoredCost = find(Pareto.cost >=245);
+ favoredScience = find(Pareto.science > .43);
 k = 1;
 for j = 1:size(favoredCost,1)
     if ismember(favoredCost(j),favoredScience) > 0
-        favoredindices(k) = favoredCost(j);
+         favoredindicesTop(k) = favoredCost(j);
         k = k+1;
     end
 end
-favoredCombinationsBot = combinations(favoredindices);
+favoredCombinationsTop = combinations(favoredindicesTop);
 
 % Plots Results
 Plot_Pareto_Analysis(Pareto, combinations);
